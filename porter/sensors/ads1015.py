b44bb6bc946176f2a7d44e6663cd58fffac7cf7a
Gabriele Coppi <gabriele.coppi@unimib.it>
2024-11-13 17:26:06 +0100

    Modified ADC code

--- porter/sensors/ads1015.py
+++ porter/sensors/ads1015.py
@@ -8,43 +8,47 @@ import smbus2
 ADS1015_REG_CONVERSION = 0x00
 ADS1015_REG_CONFIG = 0x01
 
-ADS1015_CONFIG_COMP_QUE = 0b11
+ADS1015_REG_CONFIG_CQUE_NONE = 0x0003
+ADS1015_REG_CONFIG_CLAT_NONLAT = 0x0000
+ADS1015_REG_CONFIG_CPOL_ACTVLOW = 0x0000
+ADS1015_REG_CONFIG_CMODE_TRAD = 0x0000
+ADS1015_REG_CONFIG_OS_SINGLE = 0x8000
 
 # ADS1015 config register fields
 ADS1015_CONFIG_MUX = {
-    "SINGLE_0": 0b100,
-    "SINGLE_1": 0b101,
-    "SINGLE_2": 0b110,
-    "SINGLE_3": 0b111,
-    "DIFF_0_1": 0b000,
-    "DIFF_0_3": 0b001,
-    "DIFF_1_3": 0b010,
-    "DIFF_2_3": 0b011,
+    "SINGLE_0": 0x4000,
+    "SINGLE_1": 0x5000,
+    "SINGLE_2": 0x6000,
+    "SINGLE_3": 0x7000,
+    "DIFF_0_1": 0x0000,
+    "DIFF_0_3": 0x1000,
+    "DIFF_1_3": 0x2000,
+    "DIFF_2_3": 0x3000,
 }
 
 ADS1015_CONFIG_GAIN = {
-    "6.144V": 0b000,
-    "4.096V": 0b001,
-    "2.048V": 0b010,
-    "1.024V": 0b011,
-    "0.512V": 0b100,
-    "0.256V": 0b101,
+    "2/3": 0x0000,
+    "1": 0x0200,
+    "2": 0x0400,
+    "4": 0x0600,
+    "8": 0x0800,
+    "16": 0x0A00,
 }
 
-ADS1015_CONFIG_MODE = {"Continuous": 0b0, "Single-Shot": 0b1}
+ADS1015_CONFIG_MODE = {"Continuous": 0x0000, "Single-Shot": 0x0100}
 
 ADS1015_CONFIG_RATE = {
-    "128": 0b000,
-    "250": 0b001,
-    "490": 0b010,
-    "920": 0b011,
-    "1600": 0b100,
-    "2400": 0b101,
-    "3300": 0b110,
-    "3300": 0b111,
+    "128": 0x0000,
+    "250": 0x0020,
+    "490": 0x0040,
+    "920": 0x0060,
+    "1600": 0x0080,
+    "2400": 0x00A0,
+    "3300": 0x00C0,
 }
 
 logger = logging.getLogger()
+
 
 class ADS1015:
 
@@ -56,7 +60,7 @@ class ADS1015:
 
         self.address = address
 
-        self._gain = ADS1015_CONFIG_GAIN["1.024V"]
+        self._gain = ADS1015_CONFIG_GAIN["8"]
         self._rate = ADS1015_CONFIG_RATE["1600"]
 
         self.__read_mode = kwargs.get("read mode", ADS1015_CONFIG_MODE["Continuous"])
@@ -75,33 +79,40 @@ class ADS1015:
         self.__time_sample = 1 / self._rate
 
         self.__config_register = (
-            (self.__mux_channels << 12)
-            | (self._gain << 9)
-            | (self.__read_mode << 8)
-            | (self._rate << 5)
-            | (ADS1015_CONFIG_COMP_QUE << 1)
+            ADS1015_REG_CONFIG_CQUE_NONE
+            | ADS1015_REG_CONFIG_CLAT_NONLAT
+            | ADS1015_REG_CONFIG_CPOL_ACTVLOW
+            | ADS1015_REG_CONFIG_CMODE_TRAD
+            | read_mode
+            | self._rate
+            | self._gain
+            | self.__mux_channels
+            | ADS1015_REG_CONFIG_OS_SINGLE
         )
 
         logger.info(f"Connected to ADC {self.name}")
         logger.info(f"Current Data Rate in s: {self.__time_sample}")
         logger.info(f"Current Gain: {self._gain}")
 
-    def read_continous_binary(self,  fs, flag):
+    def read_continous_binary(self, fs, flag):
 
         # Write the config register
-        self.bus.write_word_data(self.address, ADS1015_REG_CONFIG, self.__config_register)
+        self.bus.write_i2c_block_data(
+            self.address,
+            ADS1015_REG_CONFIG,
+            [(self.__config_register >> 8) & 0xFF, self.__config_register & 0xFF],
+        )
 
         while not flag.is_set():
             msg = struct.pack("<d", time.time())
 
             tstart = time.perf_counter()
 
-            raw_value = self.bus.read_word_data(self.address, ADS1015_REG_CONVERSION)
+            raw_value = self.bus.read_word_data(self.address, ADS1015_REG_CONVERSION, 2)
 
-            raw_value = ((raw_value << 8) & 0xFF00) | (raw_value >> 8)
-            raw_value = (raw_value >> 4) & 0xFFF
+            raw_value = ((result[0] << 8) | (result[1] & 0xFF)) >> 4
 
-            msg += struct.pack('<f', (raw_value * self._gain) / 2**12)
+            msg += struct.pack("<f", (raw_value * self._gain) / 2048)
 
             delta = time.perf_counter() - tstart
 
@@ -128,11 +139,15 @@ class ADS1015:
                 logger.info(f"Current Data Rate in s: {self.__time_sample}")
 
         self.__config_register = (
-            (self.__mux_channels << 12)
-            | (self._gain << 9)
-            | (self.__read_mode << 8)
-            | (self._rate << 5)
-            | (ADS1015_CONFIG_COMP_QUE << 1)
+            ADS1015_REG_CONFIG_CQUE_NONE
+            | ADS1015_REG_CONFIG_CLAT_NONLAT
+            | ADS1015_REG_CONFIG_CPOL_ACTVLOW
+            | ADS1015_REG_CONFIG_CMODE_TRAD
+            | read_mode
+            | self._rate
+            | self._gain
+            | self.__mux_channels
+            | ADS1015_REG_CONFIG_OS_SINGLE
         )
 
     def close(self):
