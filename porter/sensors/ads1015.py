1c4729d3719e7a0b987544463dd5c1e528af2be5
Gabriele Coppi <gabriele.coppi@unimib.it>
2024-11-14 13:56:17 +0100

    Moved to lgpio

--- porter/sensors/ads1015.py
+++ porter/sensors/ads1015.py
@@ -1,9 +1,9 @@
 import logging
+import random
 import struct
 import time
 
-import random
-
+import lgpio
 import smbus2
 
 # ADS1015 registers
@@ -76,9 +76,9 @@ class ADS1015:
 
         self.__mux_channels = ADS1015_CONFIG_MUX[string]
 
-        self.bus = smbus2.SMBus(bus)
+        self.bus = lgpio.i2c_open(bus, address)
 
-        self.__time_sample = 1 / 1600.
+        self.__time_sample = 1 / 1600.0
 
         self.__config_register = (
             ADS1015_REG_CONFIG_CQUE_NONE
@@ -91,47 +91,47 @@ class ADS1015:
             | self.__mux_channels
             | ADS1015_REG_CONFIG_OS_SINGLE
         )
-        
+
+        self.__read_buffer = bytearray(2)
+
         logger.info(f"Connected to ADC {self.name}")
         logger.info(f"Current Data Rate in s: {self.__time_sample}")
         logger.info(f"Current Gain: {self._gain}")
 
     def read_continous_binary(self, fs, flag):
-		
+
         string = [(self.__config_register >> 8) & 0xFF, self.__config_register & 0xFF]
         # Write the config register
-        self.bus.write_i2c_block_data(
-            self.address,
-            ADS1015_REG_CONFIG,
-            string
-        )
-        
+
+        lgpio.i2c_write_i2c_block_data(self.bus, ADS1015_REG_CONFIG, config_bytes)
+
         tmsg = time.perf_counter()
-        
+
         counter = 0
-        t=0
+        t = 0
 
         while not flag.is_set():
             msg = struct.pack("<d", time.time())
 
             tstart = time.perf_counter()
-            
-            #while time.perf_counter()- tmsg < (self.__time_sample+5e-4):
+
+            # while time.perf_counter()- tmsg < (self.__time_sample+5e-4):
             #    pass
-            raw_value = self.bus.read_i2c_block_data(self.address, ADS1015_REG_CONVERSION, 2)
+            raw_value = self.bus.i2c_read_i2c_block_data(
+                self.address, ADS1015_REG_CONVERSION, 2
+            )
             delta0 = time.perf_counter() - tstart
-            #raw_value = random.random()
+            # raw_value = random.random()
 
             raw_value = ((raw_value[0] << 8) | (raw_value[1] & 0xFF)) >> 4
 
-            msg += struct.pack("<f", (raw_value * 6.144) / 2048.)
+            msg += struct.pack("<f", (raw_value * 6.144) / 2048.0)
 
             delta = time.perf_counter() - tstart
-            
 
             msg += struct.pack("<f", delta)
 
-            #fs.write(msg)
+            # fs.write(msg)
 
             while delta < 1e-3:
                 delta = time.perf_counter() - tstart
@@ -150,33 +150,32 @@ class ADS1015:
                 self._gain = ADS1015_CONFIG_GAIN[str(config[i])]
                 logger.info(f"Current Gain: {self._gain}")
             elif i.lower() == "data_rate":
-                print('RATE', config[i], i)
+                print("RATE", config[i], i)
                 self._rate = ADS1015_CONFIG_RATE[str(config[i])]
                 self.__time_sample = 1 / config[i]
                 print(self.__time_sample)
                 logger.info(f"Current Data Rate in s: {self.__time_sample}")
-		
-        print('Gain', self._gain)
-		
+
+        print("Gain", self._gain)
+
         self.__config_register = (
             ADS1015_REG_CONFIG_CQUE_NONE
             | ADS1015_REG_CONFIG_CLAT_NONLAT
             | ADS1015_REG_CONFIG_CPOL_ACTVLOW
             | ADS1015_REG_CONFIG_CMODE_TRAD
             | self.__read_mode
-            )
-        print('Config 1' ,self.__config_register, self._rate)    
+        )
+        print("Config 1", self.__config_register, self._rate)
         self.__config_register |= self._rate
-        print('Config 2' ,self.__config_register, self._rate) 
+        print("Config 2", self.__config_register, self._rate)
         self.__config_register |= self._gain
-        print('Config 3' ,self.__config_register, self.__mux_channels) 
+        print("Config 3", self.__config_register, self.__mux_channels)
         self.__config_register |= self.__mux_channels
-        print('Config 4' ,self.__config_register) 
+        print("Config 4", self.__config_register)
         self.__config_register |= ADS1015_REG_CONFIG_OS_SINGLE
-        print('Config 5' ,self.__config_register) 
-        
-        
-        print('Config', self.__config_register)
+        print("Config 5", self.__config_register)
+
+        print("Config", self.__config_register)
 
     def close(self):
 
