91180c192be84c5ffae62ccd5c52fec62ac3fbe9
gabriele.coppi <gabriele.coppi@unimib.it>
2025-03-16 23:04:56 +0000

    Added binaries and startup scripts

---
+++ startup/i2c_startup_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+echo performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo performance | sudo tee /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo performance | sudo tee /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo performance | sudo tee /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
